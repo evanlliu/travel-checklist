3cc7488ffdf35031c693ac00d1c35cd263879502
Evan Liu <evan.liu@gmail.com>
2026-05-07 11:07:23 +0300

    1

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
-// v1.1.4
+// v1.1.5
 // 现在不需要手动修改这个文件。
 // 页面会让你填写 Cloudflare Worker 地址和访问密码，并同步保存到 data.json。
 // 如果你想预置默认值，也可以在这里填写。
 window.CHECKLIST_CONFIG = {
   API_BASE: "",
   APP_PASSWORD: "",
-  APP_VERSION: "v1.1.4"
+  APP_VERSION: "v1.1.5"
 };
--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-// v1.1.4
-const CACHE_NAME = "travel-checklist-v1.1.4";
+// v1.1.5
+const CACHE_NAME = "travel-checklist-v1.1.5";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./css/style.css?v=1.1.4",
-  "./js/config.js?v=1.1.4",
-  "./js/app.js?v=1.1.4",
-  "./manifest.json?v=1.1.4",
+  "./css/style.css?v=1.1.5",
+  "./js/config.js?v=1.1.5",
+  "./js/app.js?v=1.1.5",
+  "./manifest.json?v=1.1.5",
   "./icons/apple-touch-icon.png"
 ];
 
