76e1272ed6680d3fbfbbd5a2ff3046734b11fcf8
Evan Liu <103646150+evanlliu@users.noreply.github.com>
2026-05-11 08:45:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-// v1.5.6
-const CACHE_NAME = "travel-checklist-v1.5.6";
+// v1.6.0
+const CACHE_NAME = "travel-checklist-v1.6.0";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./css/style.css?v=1.5.6",
-  "./js/config.js?v=1.5.6",
-  "./js/app.js?v=1.5.6",
-  "./manifest.json?v=1.5.6",
+  "./css/style.css?v=1.6.0",
+  "./js/config.js?v=1.6.0",
+  "./js/app.js?v=1.6.0",
+  "./manifest.json?v=1.6.0",
   "./icons/apple-touch-icon.png"
 ];
 
